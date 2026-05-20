0708b7dd6085c28d05553607af19b193ba026295
Brennan <brennan@drake.exchange>
2026-05-20 16:10:59 -0500

    Updates

---
+++ assets/WalletLink-BSTOy_Vu-Da8FPa2c.js
@@ -0,0 +1,20 @@
+import{Cn as e,Ga as t,Lo as n,ti as r}from"./index-mAOZwWWo.js";import{t as i}from"./getFormattedUsdFromLamports-B6EqSEho-9woxQBRH.js";import{n as a}from"./transaction-CnfuREWo-NsdIh3Mg.js";import{i as o,n as s,o as c,t as l}from"./ethers-DO6hWyzN-DrmMxm8_.js";var u=n(),d=({weiQuantities:e,tokenPrice:t,tokenSymbol:n})=>{let r=o(e),i=t?c(r,t):void 0,a=l(r,n);return(0,u.jsx)(m,{children:i||a})},f=({weiQuantities:e,tokenPrice:t,tokenSymbol:n})=>{let r=o(e),i=t?c(r,t):void 0,a=l(r,n);return(0,u.jsx)(m,{children:i?(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(h,{children:`USD`}),i===`<$0.01`?(0,u.jsxs)(_,{children:[(0,u.jsx)(g,{children:`<`}),`$0.01`]}):i]}):a})},p=({quantities:e,tokenPrice:n,tokenSymbol:r=`SOL`,tokenDecimals:o=9})=>{let s=e.reduce(((e,t)=>e+t),0n),c=n&&r===`SOL`&&o===9?i(s,n):void 0,l=r===`SOL`&&o===9?a(s):`${t(s,o)} ${r}`;return(0,u.jsx)(m,{children:c?(0,u.jsx)(u.Fragment,{children:c===`<$0.01`?(0,u.jsxs)(_,{children:[(0,u.jsx)(g,{children:`<`}),`$0.01`]}):c}):l})},m=e.span`
+  font-size: 14px;
+  line-height: 140%;
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`,h=e.span`
+  font-size: 12px;
+  line-height: 12px;
+  color: var(--privy-color-foreground-3);
+`,g=e.span`
+  font-size: 10px;
+`,_=e.span`
+  display: flex;
+  align-items: center;
+`;function v(e,t){return`https://explorer.solana.com/account/${e}?chain=${t}`}var y=e=>(0,u.jsx)(b,{href:e.chainType===`ethereum`?s(e.chainId,e.walletAddress):v(e.walletAddress,e.chainId),target:`_blank`,children:r(e.walletAddress)}),b=e.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`;export{d as i,p as n,f as r,y as t};
--- assets/shouldProceedtoEmbeddedWalletCreationFlow-BJfQTiGs-DWGfJcgY.js
+++
@@ -1,1 +0,0 @@
-import{A as e,j as t}from"./index-CHU___CG.js";var n=(n,r)=>e(n,r.ethereum.createOnLogin)||t(n,r.solana.createOnLogin);export{n as t};
